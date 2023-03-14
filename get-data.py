609aba3803056ed852e99133e83ddbcd720dafa3
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2023-03-14 18:32:44 -0400

    Test

--- get-data.py
+++ get-data.py
@@ -169,8 +169,6 @@ def main():
         print("Querying site " + wl_id[0] + "...")
         start_date = pd.read_sql_query(f"SELECT max(date) as start FROM external_api_data WHERE id='{wl_id[0]}'", engine)
         start_date = pd.to_datetime(start_date.start.iat[0]) + timedelta(minutes=5)
-        print(start_date)
-        return
         new_data = get_fiman_atm(wl_id[0], 'Water Elevation', start_date, end_date)
 
         if new_data.shape[0] == 0:
