0f35d46ff61daa9593fd0603acb3e73081159788
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2023-03-14 15:12:03 -0400

    Test

--- get-data.py
+++ get-data.py
@@ -42,11 +42,11 @@ def postgres_upsert(table, conn, keys, data_iter):
     data = [dict(zip(keys, row)) for row in data_iter]
 
     insert_statement = insert(table.table).values(data)
-    upsert_statement = insert_statement.on_conflict_do_update(
-        constraint=f"{table.table.name}_pkey",
-        set_={c.key: c for c in insert_statement.excluded},
-    )
-    conn.execute(upsert_statement)
+    # upsert_statement = insert_statement.on_conflict_do_update(
+    #     constraint=f"{table.table.name}_pkey",
+    #     set_={c.key: c for c in insert_statement.excluded},
+    # )
+    conn.execute(insert_statement)
 
 #############################
 # Method-specific functions #
