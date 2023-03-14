45d17e85935768aa1dd3366b5a46b3c70aeab05a
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2023-03-14 13:14:03 -0400

    Test

--- get-data.py
+++ get-data.py
@@ -172,6 +172,7 @@ def main():
             return
         
         print(new_data.shape[0] , "new records!")
+        print(new_data, n=100)
         
         new_data.to_sql("external_api_data", engine, if_exists = "append", method=postgres_upsert, index=False)
         time.sleep(10)
